15baa015af0a0b895aba9e073e2063387a040fc7
alokvr <alokmessi@gmail.com>
2026-01-13 13:52:36 +0530

    Error fix

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,18 +4,14 @@ import { useContext } from "react";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useContext(ThemeContext);
-
   const handleThemeToggle = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme(theme == "light" ? "dark" : "light");
   };
-
   return (
-    <div className="flex items-center gap-2">
-      <span className="text-sm font-medium">
-        Theme: {theme.charAt(0).toUpperCase() + theme.slice(1)}
-      </span>
-      <Switch checked={theme === "dark"} onChange={handleThemeToggle} />
-    </div>
+    <>
+      <span>Theme : [{theme.charAt(0).toUpperCase() + theme.slice(1)}]</span>
+      <Switch onChange={handleThemeToggle} />
+    </>
   );
 };
 
--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+
 interface Itheme {
   theme: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
